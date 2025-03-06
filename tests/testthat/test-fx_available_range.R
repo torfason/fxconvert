2115468b250c6a3a4b38f1c0bb898ea825ced63b
Magnus Thor Torfason <m@zulutime.net>
2025-03-06 12:43:38 +0000

    Update tests for when data dir and database exist

--- tests/testthat/test-fx_available_range.R
+++ tests/testthat/test-fx_available_range.R
@@ -1,5 +1,7 @@
 test_that("fx_available_range() works", {
   skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
           "Package not initialized? (workspace directory missing)")
-  expect_true(FALSE)
+  fx_available_range() |>
+    expect_type("character") |>
+    expect_named(c("date_first", "date_last"))
 })
--- tests/testthat/test-fx_sitrep.R
+++ tests/testthat/test-fx_sitrep.R
@@ -1,5 +1,6 @@
 test_that("fx_sitrep() works", {
   skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
           "Package not initialized? (workspace directory missing)")
-  expect_true(FALSE)
+  fx_sitrep() |>
+    expect_output("Status report for fx package")
 })
