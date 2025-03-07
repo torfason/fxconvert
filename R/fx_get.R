5326acb8649ccf9341e57bb3255f909e9dfd29ce
Magnus Thor Torfason <m@zulutime.net>
2025-03-07 13:37:12 +0000

    Add fx_get() and related files
    - fx_get() function
    - Generated files
    - Tests
    - Package imports for purrr and dbplyr
    - Workaround for check error on dbplyr import

---
+++ R/fx_get.R
@@ -0,0 +1,104 @@
+
+#' Retrieve Foreign Exchange Rates for a Specific Date
+#'
+#' This function returns the exchange rates between two specified currencies for
+#' a given date, and calculates the exchange rate from the first specified
+#' currency to the second.
+#'
+#' @param from The ISO currency code (as a character string) for the base currency.
+#' @param to The ISO currency code (as a character string) for the target currency.
+#' @param fxdate The date for which to retrieve exchange rates, in "YYYY-MM-DD" format.
+#' @param fxsource A character string specifying the source of the exchange rate data.
+#'        Default is "ecb" (European Central Bank).
+#' @param ... Reserved
+#' @param .interpolate If a weekend date is requested, should the previous days rate
+#'   be returned. If this is false, a weekend date will result in an error.
+#'
+#' @return A numeric value representing the exchange rate from the `from` currency
+#'        to the `to` currency on the specified `fxdate`. The exchange rate
+#'        between`from` and `to`, given in a way that to convert, from `from`
+#'        to `to` one would multiply by the exchange rate. This means that the
+#'        function may not always give the expected value for well-known
+#'        currency pairs that are always quoted in the same direction.
+#' @export
+fx_get <- function(from, to, fxdate, fxsource = "ecb", ..., .interpolate = FALSE) {
+
+  # Ensure vectors are recyclable to the same length
+  args <- vctrs::vec_recycle_common(
+    from = from,
+    to = to,
+    fxdate = fxdate
+  )
+
+  # Convert recycled arguments to a tibble
+  args_tibble <- tibble::as_tibble(args)
+
+  # Apply fx_get_single over the rows of the tibble
+  purrr::pmap_vec(
+    args_tibble,
+    ~ fx_get_single(..1, ..2, ..3, fxsource = fxsource, ..., .interpolate = .interpolate),
+    .ptype = double()
+  )
+}
+
+
+
+#' This function connects to a DuckDB database containing foreign exchange rate data,
+#' retrieves exchange rates between two specified currencies for a given date,
+#' and calculates the exchange rate from the first specified currency to the second.
+#'
+#' @examples
+#' #fx_get("USD", "EUR", "2022-01-01")
+#'
+#' @rdname fx_get
+fx_get_single <- function(from, to, fxdate, fxsource = "ecb", ..., .interpolate = FALSE) {
+
+  stopifnot(length(from) == 1)
+  stopifnot(length(to) == 1)
+  stopifnot(length(fxdate) == 1)
+  stopifnot(length(.interpolate) == 1)
+
+  # Verify and preprocess parameters
+  from     <- tolower(from)
+  to       <- tolower(to)
+  fxsource <- tolower(fxsource)
+
+  # Choose whether to used the filled version of the table
+  if (.interpolate) {
+    fxtable_name = "fxtable_filled"
+  } else {
+    fxtable_name = "fxtable"
+  }
+
+  # Open con, prepare dbplyr table, and register con for closing
+  fxdata_dir <- fx_get_fxdata_dir()
+  duckdb_file <- file.path(fxdata_dir, paste0(fxsource, ".duckdb"))
+  con <- duckdb::dbConnect(duckdb::duckdb(duckdb_file, read_only = TRUE))
+  on.exit(duckdb::dbDisconnect(con, shutdown = TRUE), add = TRUE)
+  fxtable <- dplyr::tbl(con, fxtable_name)
+
+  fxdate_param <- fxdate
+  v.rates <- fxtable |>
+    dplyr::filter(fxdate == fxdate_param) |>
+    dplyr::select(dplyr::all_of(c(from, to))) |>
+    dplyr::collect() |>
+    unlist() |>
+    as.vector()
+  v.rates
+
+
+  # R does not throw an error
+  if (length(v.rates) != 2)
+    stop("length(d.rates) != 2, probably date requested was not in range or one of the rates was not found\n",
+          "(", from, ", ", to, ", ", fxdate, ")")
+
+  # We error out on NA until further notice
+  result = v.rates[2] / v.rates[1]
+  if (is.na(result))
+    stop("fx rate is NA, probably a weekend and .interpolate is false")
+
+  # Return
+  result
+}
+
+
---
+++ tests/testthat/test-fx_get.R
@@ -0,0 +1,79 @@
+
+test_that("fx_get_single() works", {
+  skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
+          "Package not initialized? (workspace directory missing)")
+
+  fx_get_single("usd", "isk", "2024-11-11") |>
+    expect_equal(139, tolerance = 1)
+
+  fx_get_single("usd", "isk", "2025-01-01") |>
+    expect_error("weekend .* .interpolate is false")
+
+  fx_get_single("usd", "isk", "2025-01-01", .interpolate = TRUE) |>
+    expect_equal(138, tolerance = 1)
+
+  fx_get_single(character(), "isk", "2024-11-11") |>
+    expect_error("length.*from")
+})
+
+test_that("fx_get() works for valid input", {
+  skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
+          "Package not initialized? (workspace directory missing)")
+
+
+  from <- c("USD", "EUR")
+  to <- c("JPY", "GBP")
+  fxdate <- c("2023-01-01", "2023-01-02")
+
+  fx_get(from, to, fxdate, .interpolate = TRUE) |>
+    expect_equal(c(131.88, 0.83), tolerance = 0.01)
+})
+
+test_that("fx_get() handles scalar or zero-length inputs correctly", {
+  skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
+          "Package not initialized? (workspace directory missing)")
+
+  from <- "USD"
+  to <- "JPY"
+  fxdate <- as.Date("2024-01-01", )
+
+  fx_get(from, to, fxdate) |>
+    expect_error("weekend .* .interpolate is false")
+
+  fx_get(from, to, fxdate, .interpolate = TRUE) |>
+    expect_equal(131, tolerance = 1)
+
+  fx_get(character(), "isk", "2024-11-11") |>
+    expect_equal(numeric())
+})
+
+test_that("fx_get throws an error for non-recyclable inputs", {
+  skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
+          "Package not initialized? (workspace directory missing)")
+
+  from <- c("USD", "EUR")
+  to <- c("JPY", "GBP", "CAD")
+  fxdate <- as.Date(c("2023-01-01", "2023-01-02"))
+
+  fx_get(from, to, fxdate) |>
+    expect_error("Can't recycle")
+})
+
+test_that("fx_get() handles input with recycling", {
+  skip_if(!fs::dir_exists(fx_get_fxdata_dir()),
+          "Package not initialized? (workspace directory missing)")
+
+  from <- "USD"
+  to <- c("JPY", "GBP")
+  fxdate <- as.Date("2023-01-01")
+
+  fx_get(from, to, fxdate, .interpolate = TRUE) |>
+    expect_equal(c(131.88, 0.83), tolerance = 0.01)
+})
+
+test_that("fx_get() handles long missing periods in the data correctly", {
+  skip("Maximum interpolation interval must be implemented")
+  fx_get(c("usd", "eur"), "isk", "2016-03-05", .interpolate = TRUE) |>
+    expect_error()
+})
+
