4aeceaf48eaed0ff3a9dc65ff629bdc9b0f98059
Magnus Thor Torfason <m@zulutime.net>
2025-05-05 16:08:37 +0000

    Add utility function read_parquet_multi() to read multiple parquet files

--- R/utils.R
+++ R/utils.R
@@ -78,3 +78,51 @@ random_date <- function(n,
 }
 
 
+#' Read and Combine Multiple Parquet Files Using nanoparquet
+#'
+#' Reads one or more Parquet files from specified directories, glob patterns,
+#' or file paths using `nanoparquet::read_parquet()`, and combines them into
+#' a single data frame using `dplyr::bind_rows()`.
+#'
+#' @param path A directory or glob pattern
+#' @param ... Additional arguments passed to `nanoparquet::read_parquet()`.
+#'
+#' @return A single data frame containing the rows from all matched Parquet files.
+#'         A `.file` column is added to indicate the source file of each row.
+#'
+#' @examples
+#' \dontrun{
+#'   df <- read_nanoparquet_multi(c("data/", "logs/*.parquet"))
+#' }
+#'
+#' @keywords internal
+#' @export
+read_parquet_multi <- function(path, ..., add_file_column = FALSE) {
+
+  # Verify input
+  checkmate::assert_string(path)
+  checkmate::assert_flag(add_file_column)
+
+  # List parquet files
+  if (fs::dir_exists(path)) {
+    parquet_files <- fs::dir_ls(path, glob = "*.parquet")
+  } else {
+    parquet_files <- fs::dir_ls(fs::path_dir(path), glob = path, fail = FALSE)
+  }
+
+  if (length(parquet_files) == 0) {
+    cli::cli_warn("No parquet files match criteria, the result will be an empty tibble")
+  }
+  l.frames <- purrr::map(parquet_files, function(x){
+    result <- nanoparquet::read_parquet(x)
+    if (add_file_column) {
+      result <- dplyr::mutate(result, .file = x, .before = 1)
+    }
+    result
+  })
+
+  # Bind the rows
+  l.frames |> dplyr::bind_rows()
+}
+
+
