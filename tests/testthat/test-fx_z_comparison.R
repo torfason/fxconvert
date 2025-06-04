0acd2a5f73e5a1b85f4e38c9126fd0e0effe0f43
Magnus Thor Torfason <m@zulutime.net>
2025-06-04 16:00:51 +0000

    Bump version to 0.3.0, and update tests and docs for fxdata v2

--- tests/testthat/test-fx_z_comparison.R
+++ tests/testthat/test-fx_z_comparison.R
@@ -7,41 +7,35 @@ test_that("same results from different banks", {
     "AUD", "NZD", "SGD", "ZAR", "HKD", "KRW", "CNY", "THB", "MXN",
     "BRL", "INR")
 
+  reps <- 20
   withr::with_seed(43, {
-    dates    <- random_date(20, "2010-01-01", "2025-03-31")
-    cur_from <- sample(curs_from_2010, 20, replace = TRUE)
-    cur_to   <- sample(curs_from_2010, 20, replace = TRUE)
+    dates    <- random_date(reps, "2010-01-01", "2025-03-31", replace = TRUE)
+    cur_from <- sample(curs_from_2010, reps, replace = TRUE)
+    cur_to   <- sample(curs_from_2010, reps, replace = TRUE)
   })
 
   d.results <- fx_get_multibank(cur_from, cur_to, dates,
                    c("ecb", "cbi", "fed"),
                    .interpolate = TRUE) |>
-    expect_output()
+    expect_s3_class("tbl_df")
 
-  d.results |>
+  d.results_snapshot <- d.results |>
     dplyr::mutate(
       hi = pmax(ecb, cbi, fed),
       lo = pmin(ecb, cbi, fed),
       delta_perc = tibble::num((hi/lo)-1, label = "%", scale = 100)
     ) |>
-    dplyr::arrange(-delta_perc) |>
-    expect_snapshot()
+    dplyr::arrange(-delta_perc)
+  d.results_snapshot |> expect_snapshot()
 
-    # # Set folder for the generated parquet files
-    # fxdata_folder <- here::here("..", "fxdata")
-    #
-    # l.d <- list()
-    # l.d[["ecb"]]  <- read_parquet_multi(here::here(fxdata_folder, "ecb"))
-    # l.d[["cbi"]]  <- read_parquet_multi(here::here(fxdata_folder, "cbi"))
-    # l.d[["fed"]] <- read_parquet_multi(here::here(fxdata_folder, "fed"))
-    # #l.d[["xfed"]] <- read_parquet_multi(here(fxdata_folder, "xfed"))
-    #
-    # d <- dplyr::inner_join(
-    #   (l.d[["ecb"]] |> dplyr::select(fxdate, sgd_ecb = sgd)),
-    #   (l.d[["fed"]] |> dplyr::select(fxdate, sgd_fed = sgd)),
-    #   by = dplyr::join_by(fxdate)
-    # )
+  # This is not run by default due to the long time it takes to run
+  # d.results_snapshot_10000 |>
+  #   dplyr::filter(delta_perc > 0.02) |>
+  #   expect_snapshot()
 
-   skip("FED data source has mixd singapore dollar and swedish Krona. That must be fixed: / SGD,DEXSDUS,Singapore Dollar to USD / SEK,DEXSIUS,Swedish Krona to USD. (This test also takes way too long)")
+  # The difference between banks should be less than 5%, with mean/median less than 0.5%
+  d.results_snapshot$delta_perc |> as.double() |> max()    |> expect_lt(0.05)
+  d.results_snapshot$delta_perc |> as.double() |> mean()   |> expect_lt(0.005)
+  d.results_snapshot$delta_perc |> as.double() |> median() |> expect_lt(0.005)
 
 })
