4ee2072a40edc180bbc4f16835bff334a9a4177b
Magnus Thor Torfason <m@zulutime.net>
2025-09-20 12:56:23 +0000

    Add git commit and push to clean_fxdata

--- build/clean_fxdata_obsolete.R
+++ build/clean_fxdata_obsolete.R
@@ -9,8 +9,11 @@
 library(here)
 library(conflicted)
 conflicts_prefer(dplyr::filter)
+library(dplyr)
 library(glue)
 library(cli)
+library(gert)
+library(stringr)
 
 # Other packages
 library(jsonlite)
@@ -21,7 +24,7 @@ library(fxconvert)
 
 # Non-packages utility files
 import::from(utils_build_fxdata.R,
-             glue_vector, fxdata_list_obsolete_files,
+             glue_vector, fxdata_list_obsolete_files, mac_alert,
              .directory = here("build"))
 
 # Set fxdata folders and banks to use
@@ -56,6 +59,73 @@ for (fxdata_folder in fxdata_folders) {
       cli_inform(c(i = "No obsolete files found for {bank}."))
     }
   }
+}
+
+# A newline at the end prevents a gap in the zay() frame
+cat("\n")
+
+
+# = = = = = = = = = = = = = =
+# Handle git commit of any changed files
+# = = = = = = = = = = = = = =
+
+
+message("Committing and pushing any updates to repo")
+
+# Set folder for the generated parquet files
+fxdata_folder <- here("..", "fxdata")
+
+# Check status of repo modifications
+d.git <- git_status(repo = fxdata_folder)
+if (nrow(d.git) > 0) {
+  cat("Listing all changed items in git status:\n")
+  print(d.git)
+}
+
+# Check status, filtering out any lines that should not be in the result
+# Only files related to the listed banks are legitimate for autocommits,
+# and only deletion operations may be performed.
+v.banks <- c("ecb", "cbi", "fed", "xfed")
+re.banks <- paste0("(", paste(v.banks, collapse = "|"), ")")
+re.dates <- "[0-9X]{4}(-[0-9]{2})?(-[0-9X]{2})?"
+d.git_status_errors <- d.git |>
+  dplyr::filter(!( str_detect(file, glue("^{re.banks}/{re.banks}_{re.dates}\\.parquet$")) & status == "deleted" ))
+
+# Check git status and panic/push/report based on status
+if (nrow(d.git_status_errors) > 0) {
+
+  # Panic if nrow(d.git_status_errors) > 0
+  cat("Errors in git status (error tibble has ", nrow(d.git_status_errors), " rows):\n")
+  print(d.git_status_errors)
+  mac_alert("R-FX Error", paste("Error in git status,", nrow(d.git_status_errors), "files in error"))
+  stop(paste("R-FX Error: Error in git status,", nrow(d.git_status_errors), "files in error"))
+
+} else if (nrow(d.git) > 0) {
+
+  # If we have rows in the status, but none of them are in error, we commit and push
+  mod_count <- nrow(filter(d.git, status == "modified"))
+  new_count <- nrow(filter(d.git, status == "new"))
+  del_count <- nrow(filter(d.git, status == "deleted"))
+  git_add(d.git$file, repo = fxdata_folder)
+  git_commit(glue("Remove obsolete parquet files on {Sys.Date()} (autocommit)"), repo = fxdata_folder) |>
+    glue_vector("Committed with ID: {.}") |>
+    print()
+  glue("Completed clean_fxdata_obsolete.R: {new_count} new files, {mod_count} changed, {del_count} deleted") |>
+    print()
+
+  cat("Preparing to push ...\n")
+  tryCatch({
+    git_push(repo = fxdata_folder)
+    cat("Push successful!\n")
+  }, error = function(e) {
+    cat("Push failed: {e$message}\n")
+  })
+
+} else {
+
+  # No changes, report and we are done
+  glue("Completed clean_fxdata_obsolete.R: No changes to repo\n") |> print()
+
 }
 
 # A newline at the end prevents a gap in the zay() frame
